b3678b83c307cdcf4c529751f1ec635ef403ff0a
mohamed-abdelkhalek <mohamedabdelkhalek1313@gmail.com>
2023-01-18 19:39:14 +0200

    Add project files.

---
+++ MVC/Program.cs
@@ -0,0 +1,16 @@
+namespace MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
